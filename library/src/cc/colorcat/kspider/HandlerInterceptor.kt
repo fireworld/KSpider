fa9c7823dc91540e9f4635846fbc178590b2edb6
cxx <xx.ch@outlook.com>
2018-02-01 17:02:30 +0800

    developing

--- library/src/cc/colorcat/kspider/HandlerInterceptor.kt
+++ library/src/cc/colorcat/kspider/HandlerInterceptor.kt
@@ -8,17 +8,30 @@ internal class HandlerInterceptor(private val spider: KSpider) : Interceptor {
 
     override fun intercept(chain: Interceptor.Chain): List<Scrap> {
         val scraps = chain.proceed(chain.seed).toMutableList()
-        val iterator = scraps.toMutableList().iterator()
-        while (iterator.hasNext()) {
-            val scrap = iterator.next()
-            if (scrap.data.isEmpty()) continue
-            var handled = false
-            spider.handlers[scrap.tag]?.forEach { if (it.handle(scrap)) handled = true }
-            if (handled) {
-                iterator.remove()
-                spider.dispatcher.handled(scrap)
+        scraps.removeIf { scrap ->
+            if (scrap.data.isEmpty()) {
+                false
+            } else {
+                var handled = false
+                spider.handlers[scrap.tag]?.forEach { if (it.handle(scrap)) handled = true }
+                if (handled) {
+                    spider.dispatcher.handled(scrap)
+                }
+                handled
             }
         }
+
+//        val iterator = scraps.toMutableList().iterator()
+//        while (iterator.hasNext()) {
+//            val scrap = iterator.next()
+//            if (scrap.data.isEmpty()) continue
+//            var handled = false
+//            spider.handlers[scrap.tag]?.forEach { if (it.handle(scrap)) handled = true }
+//            if (handled) {
+//                iterator.remove()
+//                spider.dispatcher.handled(scrap)
+//            }
+//        }
         return scraps
     }
 }
--- library/src/cc/colorcat/kspider/SeedsCleanerInterceptor.kt
+++ library/src/cc/colorcat/kspider/SeedsCleanerInterceptor.kt
@@ -8,14 +8,22 @@ internal class SeedsCleanerInterceptor(private val spider: KSpider) : Intercepto
 
     override fun intercept(chain: Interceptor.Chain): List<Scrap> {
         val scraps = chain.proceed(chain.seed).toMutableList()
-        val iterator = scraps.iterator()
-        while (iterator.hasNext()) {
-            val scrap = iterator.next()
-            if (scrap.depth > spider.maxDepth) {
-                iterator.remove()
-                spider.dispatcher.onReachedMaxDepth(scrap)
+        scraps.removeIf {
+            if (it.depth > spider.maxDepth) {
+                spider.dispatcher.onReachedMaxDepth(it)
+                true
+            } else {
+                false
             }
         }
+//        val iterator = scraps.iterator()
+//        while (iterator.hasNext()) {
+//            val scrap = iterator.next()
+//            if (scrap.depth > spider.maxDepth) {
+//                iterator.remove()
+//                spider.dispatcher.onReachedMaxDepth(scrap)
+//            }
+//        }
         return scraps
     }
 }
