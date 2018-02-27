3a3de2b40d00f41da6dbfda3f1a62577622b5114
cxx <xx.ch@outlook.com>
2018-02-27 22:23:55 +0800

    add UserAgent

---
+++ src/cc/colorcat/sample/UserAgent.kt
@@ -0,0 +1,17 @@
+package cc.colorcat.sample
+
+/**
+ * Created by cxx on 2018/2/25.
+ * xx.ch@outlook.com
+ */
+class UserAgent {
+    companion object {
+        const val NAME = "User-Agent"
+
+        const val CHROME_MAC = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36"
+        const val CHROME_WIN = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.75 Safari/537.36"
+        const val EDGE_WIN = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 Edge/16.16299"
+        const val FIREFOX_WIN = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:57.0) Gecko/20100101 Firefox/57.0"
+        const val FIREFOX_LINUX = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36"
+    }
+}
